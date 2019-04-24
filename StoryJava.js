79d839485c7a3807fee71ee3e1d3218a7c6a044d
Prutii35 <adrian.pruti@yahoo.com>
2019-04-24 07:57:41 +0300

    commit

--- StoryJava.js
+++ StoryJava.js
@@ -4,7 +4,8 @@ const array = [
         title : "The Planet",
         p1 : "It has been more than 200 years since humans have understood that the resources they had on Grolutera are no longer sufficient for their needs. Scientists all over the planet foresaw this long before it actually happened, yet few actually believed it would come to this. However, technology has finally brought humanity to the edge of the rift and the human race faces a seemingly impossible task : should it take the risk of possible destruction and jump into the abyss, should it seek uncharted territory in search for a better life...?",
         p2 : "The spatial object XRT 23B was first observed in 2142, when ISTA (INTER-GALACTIC SPACE TRAVEL ASSOCIATION) scientist and lead-researcher Dr. Anatole Zionotos discovered it on a routine scan, on the 15th of August. It appeared to be a planet fundamentally similar to Grolutera, our planet, although much larger. The fact that it hasn't been discovered earlier was incredibly strange, as it stood at an average distance of only 225 million kilometers. He immediately perceived it as a solution to our problems and began a detailed investigation to which he dedicated the rest of his life.  By the time that he died, ISTA knew just enough to begin plotting expeditions for the probing of this new object. Unfortunately, Dr. Anatole died shortly after the first mission (named Conqueror I) was launched and he did not live to witness its landing. The planet's name was then changed to Zionotos, in honor of his extraordinary initiative and work.",
-        url : "",
+        url : "images/planet.jpg",
+        url2 : "images/profesor.png",
         background : ""
     },
     {
@@ -12,49 +13,37 @@ const array = [
         title :"Expeditions",
         p1 :"The main purpose of the “Conqueror” expeditions was to understand Zionotos and its biosphere. Structurally, this strange spatial object seemed to be one huge system of underground caves and no telescope could penetrate its outer carapace. The planet did not seem able to host any life whatsoever, as Dr. Anatole concluded. Air on the surface did not hold any possibility of life sustain and underground it would be even worse. However, the planet was potentially rich in resources such as precious underground rocks. Hundreds of undiscovered species of minerals and plants were recovered on each expedition and brought to earth for studying.",
         p2 :"Locations were elaborately chosen for the landings, each one proving more rewarding than the last. Between the sites, cosmonauts constructed a network of state-of-the-art teleportation devices, called warp-gates. These deconstructed the atoms of anything that travelled through and reconstructed the object on the other side. Conqueror I laid the first warp-gate on its landing site, Site 1. Subsequent landings connected their warp-gates to the last one constructed, so each Site could be reached through multiple warpings, making resource transportation and space orientation much easier for present cosmonauts.",
-        url :"",
+        url :"images/warp.gif",
+        url2 : "",
         background :""
     },
     {
         id :3,
         title :"Conqueror V",
-        p1 :"   A crew of 13 cosmonauts was said to have been formed by ISTA for the 5th expedition, Conqueror V. More than 4 years have passed since Conqueror IV, but this mission was supposed to be larger than any of the previous ones, and consequently the media coverage was incredible. Everything went according to plan and the landing was successful. Site 5 was right next to one of the biggest cave entrances ever observed on Zionotos. Warm air flowed continuously out of the gaping nothingness and the cosmonauts set to constructing the camp. There was nothing out of the ordinary. Huge barracks were laid out like snakes on the rugged soil and the work was underway. Up until the 3rd day, the air flowing out of the cave got hotter and hotter and the planet seemed to rumble continuously, more violently as each day passed. Things worsened and on the 5th day, shrieks were heard for the first time. Some of them were hoarse, others were high-pitched, but the rumbling continued, never tiring, and the people kept on working. The next day, cosmonauts were mining some precious rock formations near the entrance into the caves. Excavations shifted towards the howling abyss and as they did, so did the reactions of the planet aggravate.",
+        p1 :"A crew of 13 cosmonauts was said to have been formed by ISTA for the 5th expedition, Conqueror V. More than 4 years have passed since Conqueror IV, but this mission was supposed to be larger than any of the previous ones, and consequently the media coverage was incredible. Everything went according to plan and the landing was successful. Site 5 was right next to one of the biggest cave entrances ever observed on Zionotos. Warm air flowed continuously out of the gaping nothingness and the cosmonauts set to constructing the camp. There was nothing out of the ordinary. Huge barracks were laid out like snakes on the rugged soil and the work was underway. Up until the 3rd day, the air flowing out of the cave got hotter and hotter and the planet seemed to rumble continuously, more violently as each day passed. Things worsened and on the 5th day, shrieks were heard for the first time. Some of them were hoarse, others were high-pitched, but the rumbling continued, never tiring, and the people kept on working. The next day, cosmonauts were mining some precious rock formations near the entrance into the caves. Excavations shifted towards the howling abyss and as they did, so did the reactions of the planet aggravate.",
         p2 :"Aerav Kyris was wielding his pneumatic pickaxe with growing weariness. They have been working since early light and the rocks got tougher each day, as they descended into the cave system below. It was as if the entire planet was sentient, refusing to give these resources away without a fight, he thought. Aerav felt the warnings kept coming since they arrived but did not dare say a thing to his colleagues, for fear of being treated as a coward. So he kept hitting, concentrating on his breathing and counting each ‘thud’ his tool made upon the hard rocks, as it helped him put his mind at ease and his worries at bay. He had long lost the current number when the gust came, and they were forced to start packing. A great storm was on the way, ISTA warned. As they were moving towards the barracks, a loud shriek pierced the air, as if the sky itself was a mere piece of cloth, ripped open by an unseen knife. They turned towards the source and realized it was the howling abyss. Then came a clicking sound, like the knocking of a thousand pebbles thrown at once into an irregular chasm, getting closer and louder each second. Terror ruled over Aerav’s body now, and he could not move anymore. He stood frozen in place, his eyes glued to the black nothingness whence the sounds came, seconds feeling like centuries, and then he saw the creatures…",
-        url :"",
+        url :"images/cave.png",
+        url2 : "images/aterizare.png",
         background :""
     },
     {
         id :4,
         title :"The Swarm",
-        p1 :"As he was watching, petrified, Aerav saw hundreds of creatures pouring out of the great cave. They seemed unified by a greater force, their coordination was absolute and all of them acted as one. His brain synapses fired up thanks to adrenaline and he turned on his heels. His cabin was not far, but the working suit made his moves heavy. Aerav glances backwards for a second, and saw his fellow cosmonauts running close behind him. He kept putting his feet into the ground, each step getting him closer to relative safety. Close to him, a worker was rushed by a spider-like creature, one which jumped on his back and pierced his suit, gnashing away relentlessly. The man’s screams were short and muffled, and he dropped dead. The creature then began sucking his blood through the hole it pierced. Immediately, a crash was heard on the left and Aerav glanced quickly to see another cosmonaut being smashed into a nearby mineral collection cart, following a crash with a horrible crab creature. The monster, greater than a man, with huge pincers, stood on its legs and shrieked horrifyingly. This scene gave Aerav wings, he leaped and dashed, dodging the jumping creatures, and finally he reached the cabin. He entered, barred the door and dropped on the floor, exhausted…",
-        p2 :"",
-        url :"",
+        p1 :"As he was watching, petrified, Aerav saw hundreds of creatures pouring out of the great cave. They seemed unified by a greater force, their coordination was absolute and all of them acted as one. All around, people clutched their plasma rifles, some started shooting, some ran away, stumbling. Aerav was stunned. He remembered his gun was in the locker still…Most of the defenders were overpowered in an instant by the monsters. The sounds were so loud, the screams so horrible, Aerav had to turn his communication off. As the creatures approached, his brain synapses fired up thanks to adrenaline and he turned on his heels. His cabin was not far, but the working suit made his moves heavy. Aerav glances backwards for a second, and saw his fellow cosmonauts running close behind him. He kept putting his feet into the ground, each step getting him closer to relative safety. Close to him, a worker was rushed by a spider-like creature, one which jumped on his back and pierced his suit, gnashing away relentlessly. The man’s screams were short and muffled, and he dropped dead. The creature then began sucking his blood through the hole it pierced. Immediately, a crash was heard on the left and Aerav glanced quickly to see another cosmonaut being smashed into a nearby mineral collection cart, following a crash with a horrible crab creature. The monster, greater than a man, with huge pincers, stood on its legs and shrieked horrifyingly. This scene gave Aerav wings, he leaped and dashed, dodging the jumping creatures, and finally he reached the cabin. He entered, barred the door and dropped on the floor, exhausted.",
+        p2 :"Kyris Aerav did not dare open his eyes, despite being awake for several hours. His whole body hurt and although he was hot and sweating, shivers ran down his spine. Outside, everything was quiet now, like it was all a dream. Aerav hoped with all his being that it would be so. He forced himself up, slowly and quietly, and glanced out the circular window of the cabin. The camp was absolutely devastated. Blue fires, fueled by nitrous oxide, peppered the ground. Other cabins were smashed to pieces. Blood gleamed on the walls of the ones that still stood. There was no sign of the creatures, nor of his colleagues. Aerav began recounting the events in his mind. By standard protocol, they received plasma guns, but who would’ve thought that they would be swarmed like this? Not to mention the fact that the other four expeditions were without an incident whatsoever. The ISTA laptop on the desk beeped. Of course they already knew. Somebody must have sent an S.O.S., he thought. Heavily, he sat on the chair and checked the message. It was from Grolutera. ISTA wanted to know if there was anyone left alive…",
+        url :"images/shooting.gif",
+        url2 : "",
         background :""
     },
     {
         id :5,
         title :"Awakening",
-        p1 :"Kyris Aerav did not dare open his eyes, despite being awake for several hours. His whole body hurt and although he was hot and sweating, shivers ran down his spine. Outside, everything was quiet now, like it was all a dream. Aerav hoped with all his being that it would be so. He forced himself up, slowly and quietly, and glanced out the circular window of the cabin. The camp was absolutely devastated. Blue fires, fueled by nitrous oxide, peppered the ground. Other cabins were smashed to pieces. Blood gleamed on the walls of the ones that still stood. There was no sign of the creatures, nor of his colleagues. Aerav began recounting the events in his mind. By standard protocol, they received plasma guns, but who would’ve thought that they would be swarmed like this? Not to mention the fact that the other four expeditions were without an incident whatsoever. The ISTA laptop on the desk beeped. Of course they already knew. Somebody must have sent an S.O.S., he thought. Heavily, he sat on the chair and checked the message. It was from Grolutera. ISTA wanted to know if there was anyone left alive…",
-        p2 :"",
-        url :"",
-        background :""
-    },
-    {
-        id :6,
-        title :"A new mission",
         p1 :"It appeared the sole survivor of the attack was indeed Aerav, and new instructions were issued. He was to leave all the resources as they were. His life was now of the utmost importance to the Association and he must be evacuated immediately, they claimed. But how? He took a quick look at the ship on which they had landed. It was flipped over, still smoking, pieces of it still scattered around the camp. He remembered the warp-gates and their system, they talked about its importance back at home. There was surely no way an EVAC ship would land on Site 5, the destruction level was incredibly high and the risk was even greater. Sites 4, Site 3 and Site 2 were all underground, as the teams explored deep into the caves. He had no equipment to access the surface from these three. Site 1 was the only solution left, but it was also the farthest. He would have to pass through all those other areas and who knew what was waiting for him down there?",
-        p2 :"",
-        url :"",
+        p2 :"To get to Site 4, he needed to descend into the cave. The laptop beeped and interrupted Aerav’s thoughts. He attached the plan and got the approval to proceed almost immediately, there was no other choice. The closet was, coincidentally, wide open and the battle-suit was gleaming with its vibrant purple colours. Next to it, his plasma rifle sat sleeping, for now. They would both become sated with alien blood, he thought. Or maybe his blood? Aerav got dressed, grabbed the gun and went outside. The warmth disappeared too, along with all other living things in the area. All around, silence and cold reigned, transforming the whole scene into an eerie painting, lifeless on a piece of dry cloth. Aerav walked to the entrance of the cave system. He believed in no God, and even if he did, what use would it be? No thing in the universe would help him down there…",
+        url :"images/run.gif",
+        url2 : "",
         background :""
-    },
-    {
-        id :7,
-        title :"Execution plan",
-        p1 :"To get to Site 4, he needed to descend into the cave. The laptop beeped and interrupted Aerav’s thoughts. He attached the plan and got the approval to proceed almost immediately, there was no other choice. The closet was, coincidentally, wide open and the battle-suit was gleaming with its vibrant purple colours. Next to it, his plasma gun sat sleeping, for now. They would both become sated with alien blood, he thought. Or maybe his blood? Aerav got dressed, grabbed the gun and went outside. The warmth disappeared too, along with all other living things in the area. All around, silence and cold reigned, transforming the whole scene into an eerie painting, lifeless on a piece of dry cloth. Aerav walked to the entrance of the cave system. He believed in no God, and even if he did, what use would it be? No thing in the universe would help him down there…",
-        p2 :"",
-        url :"",
-        background :""
-    },
+    }
 ];
 
 var index = 0;
@@ -63,7 +52,14 @@ function changePage(param){
     document.getElementsByClassName("ContainerTitlu")[0].innerHTML = array[param].title;
     document.getElementsByClassName("p1")[0].innerHTML = array[param].p1;
     document.getElementsByClassName("p2")[0].innerHTML = array[param].p2
-    //document.body.style.backgroundImage = url("images/planet.jpeg");
+    document.getElementsByClassName("gif")[0].src = array[param].url;
+    if(array[param].url2 != ""){
+        document.getElementsByClassName("gif2")[0].src = array[param].url2;
+        document.getElementsByClassName("gif2")[0].style.display = "inline-block";
+    }
+    else{
+        document.getElementsByClassName("gif2")[0].style.display = "none";
+    }
 }
 
 window.onload = function() {
